f4e1cd6daf45c05c1762e0064f679330277a96ef
daniil.leontev <75467751+daniileontev@users.noreply.github.com>
2023-05-18 16:54:21 +0500

    united communication function for all games

--- src/Engine.php
+++ src/Engine.php
@@ -3,24 +3,34 @@
 namespace BrainGames\src\Engine;
 
 /*
- * rules for games messages
+ * start communication and games
  */
 
-function brainGameRule(string $gameName): void
-{
+use function BrainGames\src\Cli\greeting;
 
+function getStart(string $gameName): void
+{
+    $userName = greeting();
     switch ($gameName) {
         case "calc":
             echo("What is the result of the expression?\n");
+            startCalcGame($userName);
             break;
         case "even":
             echo("Answer 'yes' if the number is even, otherwise answer 'no'.\n");
+            startEvenGame($userName);
             break;
         case 'gcd':
             echo("Find the greatest common divisor of given numbers.\n");
+            startGcdGame($userName);
             break;
         case "progression":
             echo("What number is missing in the progression?\n");
+            startProgressionGame($userName);
+            break;
+        case "prime":
+            echo ("Answer 'yes' if given number is prime. Otherwise answer 'no'.\n");
+            startPrimeGame($userName);
             break;
         default:
             echo "Unknown game specified!\n";
@@ -76,4 +86,23 @@ function dividerCheck($num1, $num2)
         $num2 = $tmp;
     }
     return $num1;
+}
+/*
+ * brain-prime functions
+ */
+
+function isPrime($num): bool
+{
+    $n = 0;
+    for ($i = 2; $i < ($num / 2 + 1); $i++) {
+        if ($num % $i == 0) {
+            $n++;
+            break;
+        }
+    }
+    if ($n == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
